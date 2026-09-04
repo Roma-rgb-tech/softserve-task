ua-academy-projects/push-and-pray

Make the deployment portable between GCP and AWS
The cloud a VM runs in becomes a field in the project configuration instead
of a property of the code. `default_cloud` covers every VM and an optional
per-VM `cloud` overrides it, so one configuration file decides where the
stack goes up and the same roles and playbooks deploy it either way.

Terraform
- catalog.tf holds one translation table per portable token - size, disk
  type, region, zone, os - keyed by cloud, resolved with lookup(). The
  configuration says `size: micro`, never `e2-micro` or `t3.micro`.
- validation.tf checks every token before a provider is called, so a typo
  names itself instead of surfacing as an index error inside a module.
- modules/{network,vm} become modules/gcp/{network,vm}, and modules/aws/
  gains the same two. Both VM modules take the same variables and return
  the same outputs; network tags versus security groups, service-account
  email versus IAM role, image family versus AMI lookup stay inside.
- An AMI ID is not hardcoded anywhere: the portable `os` token resolves to
  a name pattern the AWS module resolves with a data source.
- Resources for a cloud are created only when a VM targets it, through
  for_each over a filtered map rather than a ternary.
- modules/gcp/vm/locals.tf is dropped: nothing referenced the cloud-init it
  built, and the three variables that fed it cannot be part of an interface
  shared with AWS. The templates themselves are untouched and move with the
  module; pr-validation.yml follows them to the new path.

Ansible
- oilscope_gcp.py becomes oilscope.py: it now reads the portable schema,
  works out which clouds the configuration declares and delegates to
  gcp_compute, aws_ec2 or both into one inventory. Group names, composed
  variables and the bastion port behaviour are unchanged.
- resolve_secrets and secret_versions split by cloud. The AWS paths keep the
  guarantees of the GCP ones: values through stdin, never an argument;
  no_log on every task th

infrastructure/terraform/outputs.tf
@@ -1,12 +1,31 @@
+output "clouds" {
+  description = "Clouds this state has resources in."
+  value       = sort(local.clouds)
+}
+
+output "workload_clouds" {
+  description = "Cloud each VM was created in."
+  value = {
+    for name, vm in local.vms : name => vm.cloud
+  }
+}
+
+output "regions" {
+  description = "Provider region per cloud in use, resolved from the portable location token."
+  value = {
+    for cloud in local.clouds : cloud => local.region[cloud]
+  }
+}
+
 output "bastion_public_ip" {
   description = "Bastion public IP."
-  value       = module.vm["bastion"].public_ip
+  value       = local.vm["bastion"].public_ip
 }
 
 output "workload_vm_names" {
   description = "VM names by workload."
   value = {
-    for name, workload in local.workload_vms : name => module.vm[name].name
+    for name, workload in local.workload_vms : name => local.vm[name].name
   }
 }
 
@@ -20,45 +39,46 @@ output "workload_roles" {
 output "workload_internal_ips" {
   description = "Internal IPs by workload."
   value = {
-    for name, workload in local.workload_vms : name => module.vm[name].internal_ip
+    for name, workload in local.workload_vms : name => local.vm[name].internal_ip
   }
 }
 
 output "workload_external_ips" {
   description = "External IPs by workload."
   value = {
-    for name, workload in local.workload_vms : name => module.vm[name].public_ip
+    for name, workload in local.workload_vms : name => local.vm[name].public_ip
   }
 }
 
-output "workload_network_tags" {
-  description = "Network tags by workload."
+output "workload_network_groups" {
+  description = "Network group identifiers by workload. Compute Engine network tags on GCP; security group IDs on AWS."
   value = {
-    for name, workload in local.workload_vms : name => module.vm[name].network_tags
+    for name, workload in local.workload_vms : name => local.vm[name].network_groups
   }
 }
 
-output "workload_service_account_emails" {
-  description = "Service-account emails by workload."
+output "workload_runtime_identities" {
+  description = "Identity each workload runs as. Service-account emails on GCP; IAM role names on AWS."
   value = {
-    for name, workload in local.workload_vms : name => module.vm[name].service_account_email
+    for name, workload in local.workload_vms : name => local.vm[name].runtime_identity
   }
 }
 
 output "secret_ids" {
-  description = "Secret Manager container IDs created from the project configuration."
+  description = "Secret container IDs created from the project configuration."
   value       = sort(local.all_secret_ids)
 }
 
 output "secret_resource_names" {
-  description = "Fully qualified Secret Manager resource names, by secret ID."
-  value = {
-    for secret_id, secret in google_secret_manager_secret.this : secret_id => secret.name
-  }
+  description = "Fully qualified secret resource names, by secret ID. Never values."
+  value = merge(
+    { for secret_id, secret in google_secret_manager_secret.this : secret_id => secret.name },
+    { for secret_id, secret in aws_secretsmanager_secret.this : secret_id => secret.arn },
+  )
 }
 
 output "workload_secret_access" {
-  description = "Secret IDs each workload service account may read. Names only - never values."
+  description = "Secret IDs each workload runtime identity may read. Names only - never values."
   value = {
     for name, workload in local.workload_vms :
     name => sort(distinct(values(workload.secret_mappings)))

Roma-rgb-tech/softserve-task

infrastructure/terraform/outputs.tf
@@ -1,66 +1,62 @@
+output "clouds" {
+  description = "Clouds this state has resources in."
+  value       = sort(local.clouds)
+}
+
+output "regions" {
+  description = "Provider region per cloud in use, resolved from the portable default_region token."
+  value = {
+    for cloud, region in { gcp = module.gcp.region, aws = module.aws.region } :
+    cloud => region if region != null
+  }
+}
+
 output "bastion_public_ip" {
   description = "Bastion public IP."
-  value       = module.vm["bastion"].public_ip
+  value       = one([for name, vm in local.vms : vm.public_ip if vm.role == "bastion"])
 }
 
 output "workload_vm_names" {
   description = "VM names by workload."
-  value = {
-    for name, workload in local.workload_vms : name => module.vm[name].name
-  }
+  value       = { for name, vm in local.vms : name => vm.name if vm.role != "bastion" }
 }
 
 output "workload_roles" {
   description = "Roles by workload."
-  value = {
-    for name, workload in local.workload_vms : name => workload.role
-  }
+  value       = { for name, vm in local.vms : name => vm.role if vm.role != "bastion" }
 }
 
 output "workload_internal_ips" {
   description = "Internal IPs by workload."
-  value = {
-    for name, workload in local.workload_vms : name => module.vm[name].internal_ip
-  }
+  value       = { for name, vm in local.vms : name => vm.internal_ip if vm.role != "bastion" }
 }
 
 output "workload_external_ips" {
   description = "External IPs by workload."
-  value = {
-    for name, workload in local.workload_vms : name => module.vm[name].public_ip
-  }
+  value       = { for name, vm in local.vms : name => vm.public_ip if vm.role != "bastion" }
 }
 
-output "workload_network_tags" {
-  description = "Network tags by workload."
-  value = {
-    for name, workload in local.workload_vms : name => module.vm[name].network_tags
-  }
+output "workload_network_groups" {
+  description = "Network group identifiers by workload. Compute Engine network tags on GCP; security group IDs on AWS."
+  value       = { for name, vm in local.vms : name => vm.network_groups if vm.role != "bastion" }
 }
 
-output "workload_service_account_emails" {
-  description = "Service-account emails by workload."
-  value = {
-    for name, workload in local.workload_vms : name => module.vm[name].service_account_email
-  }
-}
-
-output "secret_ids" {
-  description = "Secret Manager container IDs created from the project configuration."
-  value       = sort(local.all_secret_ids)
-}
-
-output "secret_resource_names" {
-  description = "Fully qualified Secret Manager resource names, by secret ID."
-  value = {
-    for secret_id, secret in google_secret_manager_secret.this : secret_id => secret.name
-  }
+output "workload_runtime_identities" {
+  description = "Identity each workload runs as. Service-account emails on GCP; IAM role names on AWS."
+  value       = { for name, vm in local.vms : name => vm.runtime_identity if vm.role != "bastion" }
 }
 
 output "workload_secret_access" {
-  description = "Secret IDs each workload service account may read. Names only - never values."
-  value = {
-    for name, workload in local.workload_vms :
-    name => sort(distinct(values(workload.secret_mappings)))
-  }
+  description = "Secret IDs each workload runtime identity may read. Names only - never values."
+  value       = { for name, vm in local.vms : name => vm.secret_access if vm.role != "bastion" }
+}
+
+output "secret_ids" {
+  description = "Secret container IDs created from the project configuration."
+  value       = sort(distinct(concat(keys(module.gcp.secret_resource_names), keys(module.aws.secret_resource_names))))
+}
+
+output "secret_resource_names" {
+  description = "Fully qualified secret resource names, by secret ID. Never values."
+  value       = merge(module.gcp.secret_resource_names, module.aws.secret_resource_names)
 }